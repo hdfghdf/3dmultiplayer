088fb468408d57ccd02e90d5212e586c1b4bf068
hdfghdf <baoshenglooi@gmail.com>
2025-05-12 22:13:03 +0800

    Fix socket event handling for name change

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
@@ -14,15 +13,20 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
+  // Set initial player position
   players[socket.id] = {
     x: Math.random() * 10,
     y: 0,
     z: Math.random() * 10,
   };
 
+  // Send the current players to the new player
   socket.emit("currentPlayers", players);
+
+  // Broadcast the new player to all other players
   socket.broadcast.emit("newPlayer", { id: socket.id, position: players[socket.id] });
 
+  // Handle player movement
   socket.on("move", (position) => {
     if (players[socket.id]) {
       players[socket.id] = position;
@@ -30,6 +34,13 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Handle name change event
+  socket.on("changeName", (newName) => {
+    console.log(`Name changed for ${socket.id} to ${newName}`);
+    io.emit("updateName", { id: socket.id, name: newName });
+  });
+
+  // Handle player disconnect
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("playerDisconnected", socket.id);
@@ -37,8 +48,5 @@ io.on("connection", (socket) => {
   });
 });
 
-socket.on("changeName", (newName) => {
-  io.emit("updateName", { id: socket.id, name: newName });
-});
-
+// Start the server
 server.listen(3000, () => console.log("Server running on http://localhost:3000"));
